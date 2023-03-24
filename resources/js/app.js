292fc63856b68c2ecdeb84986e115250d62be45f
Brandon Vasquez <thatsjaskett@gmail.com>
2023-03-24 15:24:36 +0100

    Modifiche layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
+import '~resources/scss/app.scss';
+import * as bootstrap from 'bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+import.meta.glob([
+    '../img/**'
+])
